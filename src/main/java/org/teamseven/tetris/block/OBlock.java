c7c6f765aecde44ef17fc3841b1dca783d1c4215
wlsh44 <k87913j@gmail.com>
2022-04-03 16:57:02 +0900

    feat: 기존 레퍼런스 블럭 코드 추가 #4

---
+++ src/main/java/org/teamseven/tetris/block/OBlock.java
@@ -0,0 +1,14 @@
+package org.teamseven.tetris.block;
+
+import java.awt.Color;
+
+public class OBlock extends Block {
+
+    public OBlock() {
+        shape = new int[][] {
+                {1, 1},
+                {1, 1}
+        };
+        color = Color.YELLOW;
+    }
+}
