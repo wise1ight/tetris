8df065d5909ea9bb4b51c0771ec9f70c3b4918fc
김현욱 <company@wiselight.kr>
2022-04-03 16:30:51 +0900

    test: 예시용 '항상 통과하는 테스트코드' 작성 #2

---
+++ src/test/java/org/teamseven/tetris/AlwaysPassTest.java
@@ -0,0 +1,13 @@
+package org.teamseven.tetris;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AlwaysPassTest {
+
+    @Test
+    void demoTestMethod() {
+        assertTrue(true);
+    }
+}
