9ac4cba228b820d1c721f4febc8efb9366153647
isTest001 <91168781+isTest001@users.noreply.github.com>
2022-04-16 15:22:43 +0900

    feat: 테트리스 화면 #9

--- src/main/java/org/teamseven/tetris/ui/startmenu/MainFrame.java
+++ src/main/java/org/teamseven/tetris/ui/startmenu/MainFrame.java
@@ -7,15 +7,15 @@ public class MainFrame extends BaseFrame {
     private JPanel scenes;
     private CardLayout cardLayout;
     private StartMenuPane startMenu;
-    private JLayeredPane jl1;
+    private TetrisPane tetris;
     private CardSwitcher cardSwitcher;
 
     private int[] resolution = new int[2];           // frame resolution
     private int[] preferredResolution = new int[2];  // frame resolution - frame top border
 
     public MainFrame() {
-        resolution[0] = 1200;
-        resolution[1] = 600;
+        resolution[0] = 1600;
+        resolution[1] = 900;
         int[] frameBorderSize = new int[2];       // frame top border
         frameBorderSize[0] = this.getInsets().left + this.getInsets().right;
         frameBorderSize[1] = this.getInsets().top + this.getInsets().bottom;
@@ -31,30 +31,16 @@ public class MainFrame extends BaseFrame {
         scenes = new JPanel(cardLayout);
         cardSwitcher = new CardSwitcher(scenes, cardLayout);
         startMenu = new StartMenuPane(cardSwitcher, preferredResolution);
-        jl1 = new JLayeredPane();
+        tetris = new TetrisPane(cardSwitcher, preferredResolution);
     }
 
     @Override
     public void setDesign() {
-        // temporary scene (delete lator)
-        jl1.setOpaque(true);
-        JButton jButton1 = new JButton("Open");
-        jButton1.setBackground(Color.green);
-        jButton1.setBounds(175, 50, 150, 100);
-        jl1.add(jButton1, JLayeredPane.DEFAULT_LAYER);
-        JButton jButton2 = new JButton("Open");
-        jButton2.setBackground(Color.BLUE);
-        jButton2.setBounds(300, 100, 150, 100);
-        jl1.add(jButton2, JLayeredPane.POPUP_LAYER);
-        jl1.setPreferredSize(new Dimension(300, 310));
-        jl1.add(new JLabel("test"), 300);
-        jl1.add(new JButton("d"), 300);
-        jl1.setBackground(Color.orange);
 
         //set scenes
         scenes.setPreferredSize(new Dimension(preferredResolution[0], preferredResolution[1]));
         scenes.add(startMenu, "startMenu");
-        scenes.add(jl1, "jl1");
+        scenes.add(tetris, "tetris");
         cardSwitcher.switchCard("startMenu");
 
         //set MainFrame
---
+++ src/main/java/org/teamseven/tetris/ui/startmenu/TetrisPane.java
@@ -0,0 +1,95 @@
+package org.teamseven.tetris.ui.startmenu;
+
+import javax.swing.*;
+import javax.swing.border.CompoundBorder;
+import java.awt.*;
+
+public class TetrisPane extends JLayeredPane implements IDesign {
+    private JPanel main;
+    private JTextPane tetrisBoard, currBlockBoard, scoreBoard;
+
+    private GridBagConstraints gridBagConstraints;
+    private GridBagLayout gridBagLayout;
+
+    private int[] preferredResolution;  // frame resolution - frame top border
+    private CardSwitcher cardSwitcher;
+
+    public TetrisPane(CardSwitcher cardSwitcher, int[] preferredResolution) {
+        this.preferredResolution = preferredResolution;
+        this.cardSwitcher = cardSwitcher;
+
+        setComp();
+        setDesign();
+        setAction();
+    }
+
+    @Override
+    public void setComp() {
+        main = new JPanel();
+        tetrisBoard = new JTextPane();
+        currBlockBoard = new JTextPane();
+        scoreBoard = new JTextPane();
+
+        gridBagConstraints = new GridBagConstraints();
+        gridBagLayout = new GridBagLayout();
+    }
+
+    @Override
+    public void setDesign() {
+        tetrisBoard.setEditable(false);
+        currBlockBoard.setEditable(false);
+        scoreBoard.setEditable(false);
+
+        CompoundBorder border = BorderFactory.createCompoundBorder(
+                BorderFactory.createLineBorder(Color.gray, preferredResolution[1] / 60),
+                BorderFactory.createLineBorder(Color.darkGray, preferredResolution[1] / 90));
+
+        tetrisBoard.setBorder(border);
+        currBlockBoard.setBorder(border);
+        scoreBoard.setBorder(border);
+
+        main.setLayout(gridBagLayout);
+        gridBagConstraints.fill = GridBagConstraints.BOTH;
+
+        gridBagConstraints.weightx = 2.0;
+        gridBagConstraints.weighty = 3.0;
+        gridBagConstraints.insets = new Insets(preferredResolution[1] / 18, preferredResolution[0] * 3 / 8, preferredResolution[1] / 18, 0);
+        make(tetrisBoard, 0, 0, 1, 2);
+
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.insets = new Insets(preferredResolution[1] / 18, preferredResolution[0] / 16, preferredResolution[1] / 180, preferredResolution[0] * 3 / 16);
+        make(currBlockBoard, 1, 0, 1, 1);
+
+        gridBagConstraints.weighty = 1.0;
+        gridBagConstraints.insets = new Insets(preferredResolution[1] / 180, preferredResolution[0] / 16, preferredResolution[1] * 3 / 5, preferredResolution[0] * 3 / 16);
+        make(scoreBoard, 1, 1, 1, 1);
+
+        main.add(tetrisBoard);
+        main.add(currBlockBoard);
+        main.add(scoreBoard);
+
+        main.setBounds(0, 0, preferredResolution[0], preferredResolution[1]);
+        this.add(main, JLayeredPane.DEFAULT_LAYER);
+
+    }
+
+    @Override
+    public void setAction() {
+
+    }
+
+    public void make(JComponent c, int x, int y, int w, int h) {
+        gridBagConstraints.gridx = x;
+        gridBagConstraints.gridy = y;
+        gridBagConstraints.gridwidth = w;
+        gridBagConstraints.gridheight = h;
+        gridBagLayout.setConstraints(c, gridBagConstraints);
+
+        // for test
+
+
+        // c.setBackground(Color.getHSBColor((y+1)  /9f + (x+1) /9f,0.75f,0.95f));
+        //  gridBagConstraints.insets = new Insets(15,10,15,10);
+
+    }
+}
