7ac1286c9ed67f47c7926435ab97c2d6369e5d60
isTest001 <91168781+isTest001@users.noreply.github.com>
2022-04-16 15:23:26 +0900

    feat: 모드 UI #9

--- src/main/java/org/teamseven/tetris/ui/startmenu/StartMenuPane.java
+++ src/main/java/org/teamseven/tetris/ui/startmenu/StartMenuPane.java
@@ -1,17 +1,20 @@
 package org.teamseven.tetris.ui.startmenu;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import javax.swing.plaf.FontUIResource;
 import java.awt.*;
 import java.util.HashSet;
 import java.util.Set;
 
 public class StartMenuPane extends JLayeredPane implements IDesign {
     private JLabel title;
-    private CustomButton startButton, settingButton, scoreButton, exitButton;
+    private CustomButton startButton, settingButton, scoreButton, exitButton, noItemModeButton, ItemModeButton;
     private JButton settingBoard_ExitButton, scoreBoard_ExitButton;                         // exit button on popup panel
     private JPanel main;                                                                    // main panel on startMenuPane (DEFAULT_LAYER)     
     private JPanel titlePanel, buttonPannel1, buttonPannel2, buttonPannel3, buttonPannel4;  // BoardLayout, panels on main panel
-    private JPanel settingBoard, scoreBoard;                                                // scoreBoard panel on startMenuPane (POPUP_LAYER)
+    private JPanel settingBoard, scoreBoard, modeBoard;                                                // scoreBoard panel on startMenuPane (POPUP_LAYER)
     private GridBagConstraints gridBagConstraints;
     private GridBagLayout gridBagLayout;
 
@@ -41,16 +44,24 @@ public class StartMenuPane extends JLayeredPane implements IDesign {
         buttonPannel3 = new JPanel(new BorderLayout());
         buttonPannel4 = new JPanel(new BorderLayout());
 
+        buttonPannel1.setPreferredSize(new Dimension(0, preferredResolution[1] / 20));
+        buttonPannel2.setPreferredSize(new Dimension(0, preferredResolution[1] / 20));
+        buttonPannel3.setPreferredSize(new Dimension(0, preferredResolution[1] / 20));
+        buttonPannel4.setPreferredSize(new Dimension(0, preferredResolution[1] / 20));
+
         title = new JLabel("TETRIS");
         startButton = new CustomButton();
         settingButton = new CustomButton();
         scoreButton = new CustomButton();
         exitButton = new CustomButton();
+        noItemModeButton = new CustomButton();
+        ItemModeButton = new CustomButton();
 
         settingBoard_ExitButton = new JButton();
         scoreBoard_ExitButton = new JButton();
         scoreBoard = new JPanel();
         settingBoard = new JPanel();
+        modeBoard = new JPanel(new GridLayout(1, 2, preferredResolution[0] / 32, 0));
 
         gridBagConstraints = new GridBagConstraints();
         gridBagLayout = new GridBagLayout();
@@ -65,33 +76,45 @@ public class StartMenuPane extends JLayeredPane implements IDesign {
         gridBagConstraints.weightx = 4.0;
         gridBagConstraints.weighty = 4.0;
 
-        gridBagConstraints.insets = new Insets(15, 250, 15, 250);
+        gridBagConstraints.insets = new Insets(preferredResolution[1] / 40, 0, preferredResolution[1] / 40, 0);
         make(titlePanel, 1, 0, 1, 1);
 
         gridBagConstraints.weighty = 0.1;
 
-        gridBagConstraints.insets = new Insets(15, 10, 15, 10);
+        gridBagConstraints.insets = new Insets(preferredResolution[1] / 40, 0, preferredResolution[1] / 40, 0);
         make(buttonPannel1, 1, 2, 1, 1);
 
-        gridBagConstraints.insets = new Insets(15, 10, 15, 10);
+        gridBagConstraints.insets = new Insets(preferredResolution[1] / 40, 0, preferredResolution[1] / 40, 0);
         make(buttonPannel2, 1, 4, 1, 1);
 
-        gridBagConstraints.insets = new Insets(15, 10, 15, 10);
+        gridBagConstraints.insets = new Insets(preferredResolution[1] / 40, 0, preferredResolution[1] / 40, 0);
         make(buttonPannel3, 1, 6, 1, 1);
 
-        gridBagConstraints.insets = new Insets(15, 330, 30, 330);
+        gridBagConstraints.insets = new Insets(preferredResolution[1] / 40, 0, preferredResolution[1] / 20, 0);
         make(buttonPannel4, 1, 8, 1, 1);
 
 
         // set Label, Button
-        title.setFont(new Font(title.getFont().getName(), title.getFont().getStyle(), preferredResolution[1] * 1 / 5));
+        title.setFont(new Font(title.getFont().getName(), title.getFont().getStyle(), preferredResolution[0] * 1 / 10));
         title.setHorizontalAlignment(JLabel.CENTER);
 
+
         startButton.setText("시작");
+
         settingButton.setText("설정");
         scoreButton.setText("스코어보드");
         exitButton.setText("게임 종료 메뉴");
         exitButton.setText("끄기");
+        noItemModeButton.setText("no Item Mode");
+        noItemModeButton.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY, preferredResolution[1] / 90));
+        noItemModeButton.setBorderPainted(true);
+
+
+        ItemModeButton.setText("Item Mode");
+        ItemModeButton.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY, preferredResolution[1] / 90));
+        ItemModeButton.setBorderPainted(true);
+
+
         settingBoard_ExitButton.setPreferredSize(new Dimension(50, 50));
         scoreBoard_ExitButton.setPreferredSize(new Dimension(50, 50));
 
@@ -105,6 +128,8 @@ public class StartMenuPane extends JLayeredPane implements IDesign {
 
         scoreBoard.add(scoreBoard_ExitButton);
         settingBoard.add(settingBoard_ExitButton);
+        modeBoard.add(noItemModeButton);
+        modeBoard.add(ItemModeButton);
 
         main.add(titlePanel);
         main.add(buttonPannel1);
@@ -119,7 +144,12 @@ public class StartMenuPane extends JLayeredPane implements IDesign {
         scoreBoard.setBackground(Color.GREEN);
         scoreBoard.setBounds(preferredResolution[0] * 1 / 10, preferredResolution[1] * 1 / 10,
                 preferredResolution[0] * 8 / 10, preferredResolution[1] * 8 / 10);
-
+        modeBoard.setBackground(Color.white);
+        modeBoard.setBounds(preferredResolution[0] * 1 / 10, preferredResolution[1] * 1 / 10,
+                preferredResolution[0] * 8 / 10, preferredResolution[1] * 8 / 10);
+        modeBoard.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.lightGray, preferredResolution[1] / 180),
+                BorderFactory.createEmptyBorder(preferredResolution[1] / 15, preferredResolution[1] / 15,
+                        preferredResolution[1] / 15, preferredResolution[1] / 15)));
 
         // set main pannel on StartMenuPane
         main.setBounds(0, 0, preferredResolution[0], preferredResolution[1]);
@@ -133,8 +163,15 @@ public class StartMenuPane extends JLayeredPane implements IDesign {
 
         // set ButtonAction
         startButton.addActionListener(e -> {
-            System.out.println(e.getActionCommand());
-            cardSwitcher.switchCard("jl1");
+            //System.out.println(e.getActionCommand());
+            //cardSwitcher.switchCard("jl1");
+            this.add(modeBoard, JLayeredPane.POPUP_LAYER);
+            noItemModeButton.requestFocus();
+            startButton.setFocusable(false);
+            settingButton.setFocusable(false);
+            scoreButton.setFocusable(false);
+            exitButton.setFocusable(false);
+
         });
         settingButton.addActionListener(e -> {
             this.add(settingBoard, JLayeredPane.POPUP_LAYER);
@@ -157,6 +194,9 @@ public class StartMenuPane extends JLayeredPane implements IDesign {
             this.repaint();
             scoreButton.requestFocus();
         });
+        noItemModeButton.addActionListener(e -> {
+            cardSwitcher.switchCard("tetris");
+        });
 
     }
 
@@ -169,10 +209,10 @@ public class StartMenuPane extends JLayeredPane implements IDesign {
 
         // for test
 
-        /*
-        c.setBackground(Color.getHSBColor((y-1)/9f,0.75f,0.95f));
-        gridBagConstraints.insets = new Insets(15,10,15,10);
-         */
+
+        //c.setBackground(Color.getHSBColor((y-1)/9f,0.75f,0.95f));
+        //  gridBagConstraints.insets = new Insets(15,10,15,10);
+
     }
 
     // TAB -> up,down key
@@ -192,5 +232,6 @@ public class StartMenuPane extends JLayeredPane implements IDesign {
         BackwardKey.add(UpKeyStroke);
         KeyboardFocusManager.getCurrentKeyboardFocusManager().setDefaultFocusTraversalKeys(KeyboardFocusManager.BACKWARD_TRAVERSAL_KEYS, BackwardKey);
     }
+
 
 }
