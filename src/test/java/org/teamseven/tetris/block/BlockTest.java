9520248ae0f12fcb5e4ddd12fd2a0ccb695b49cc
wlsh44 <k87913j@gmail.com>
2022-04-17 16:41:54 +0900

    test: rotate 테스트 코드 추가 #4

---
+++ src/test/java/org/teamseven/tetris/block/BlockTest.java
@@ -0,0 +1,80 @@
+package org.teamseven.tetris.block;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BlockTest {
+
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    @Nested
+    class RotateTest {
+
+        @ParameterizedTest(name = "블럭 회전")
+        @MethodSource("blockProvider")
+        void rotate(Block block, int[][] expected) {
+            block.right_rotate();
+
+            assertThat(block.shape).isDeepEqualTo(expected);
+        }
+
+
+        private Stream<Arguments> blockProvider() {
+            return Stream.of(
+                    Arguments.of(new IBlock(), new int[][] {{1},
+                                                            {1},
+                                                            {1},
+                                                            {1}}),
+                    Arguments.of(new JBlock(), new int[][] {{0, 1},
+                                                            {0, 1},
+                                                            {1, 1}}),
+                    Arguments.of(new LBlock(), new int[][] {{1, 1},
+                                                            {0, 1},
+                                                            {0, 1}}),
+                    Arguments.of(new OBlock(), new int[][] {{1, 1},
+                                                            {1, 1}}),
+                    Arguments.of(new SBlock(), new int[][] {{1, 0},
+                                                            {1, 1},
+                                                            {0, 1}}),
+                    Arguments.of(new TBlock(), new int[][] {{1, 0},
+                                                            {1, 1},
+                                                            {1, 0}}),
+                    Arguments.of(new ZBlock(), new int[][] {{0, 1},
+                                                            {1, 1},
+                                                            {1, 0}}));
+        }
+    }
+
+
+    @Test
+    @DisplayName("I블럭 회전 테스트")
+    void IBlock_rotate() {
+        int[][] expected = new int[][]{{1}, {1}, {1}, {1}};
+        Block block = new IBlock();
+
+        block.right_rotate();
+
+        assertThat(block.shape).isDeepEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("J블럭 회전 테스트")
+    void rotate() {
+        int[][] expected = new int[][]{{0, 1},
+                                       {0, 1},
+                                       {1, 1}};
+        Block block = new JBlock();
+
+        block.right_rotate();
+
+        assertThat(block.shape).isDeepEqualTo(expected);
+    }
+}
