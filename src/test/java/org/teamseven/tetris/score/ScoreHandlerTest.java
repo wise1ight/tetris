2f521c8b5cbb2bb8c6de880f58d7d7a8993c6879
wlsh44 <k87913j@gmail.com>
2022-04-19 00:01:13 +0900

    test: score 정렬 테스트 추가 #16

--- src/test/java/org/teamseven/tetris/score/ScoreHandlerTest.java
+++ src/test/java/org/teamseven/tetris/score/ScoreHandlerTest.java
@@ -1,5 +1,6 @@
 package org.teamseven.tetris.score;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -15,6 +16,12 @@ class ScoreHandlerTest {
     ScoreHandler handler = new ScoreHandler();
     String fileName = "test.csv";
 
+    @AfterEach
+    void deleteAll() {
+        File file = new File(SCORE_ROOT + fileName);
+        file.delete();
+    }
+
     @Test
     @DisplayName("파일 저장 및 불러오기 테스트")
     void file_save_test() {
@@ -27,6 +34,23 @@ class ScoreHandlerTest {
 
         assertThat(after.size()).isEqualTo(before.size() + 1);
         assertThat(after.contains(score)).isTrue();
+    }
+
+    @Test
+    @DisplayName("점수 내림차순 이름 오름차순 정렬 테스트")
+    void score_descending_name_ascending_test() {
+        Score score1 = new Score(100, "AAA");
+        Score score2 = new Score(200, "AAA");
+        Score score3 = new Score(100, "AAB");
+
+        handler.saveScoreFile(score1, fileName);
+        handler.saveScoreFile(score2, fileName);
+        handler.saveScoreFile(score3, fileName);
+        List<Score> scores = handler.getScores(fileName);
+
+        assertThat(scores.get(0)).isEqualTo(score2);
+        assertThat(scores.get(1)).isEqualTo(score1);
+        assertThat(scores.get(2)).isEqualTo(score3);
     }
 
     @Test
