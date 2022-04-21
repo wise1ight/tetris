0f0efccc3504a501813da374ea3dd87b4d46100c
wlsh44 <k87913j@gmail.com>
2022-04-22 01:37:29 +0900

    feat: BoomBlock 테스트 코드 추가 #42

---
+++ src/test/java/org/teamseven/tetris/block/item/BoomBlockTest.java
@@ -0,0 +1,85 @@
+package org.teamseven.tetris.block.item;
+
+import org.junit.jupiter.api.Test;
+import org.teamseven.tetris.Board.GameBoard;
+import org.teamseven.tetris.block.CurrBlock;
+import org.teamseven.tetris.block.UnitBlock;
+
+import java.awt.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BoomBlockTest {
+
+    CurrBlock curr;
+    GameBoard board = new GameBoard();
+
+    UnitBlock[][] res = initBoard(new int[][]{
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+            {0, 0, 1, 1, 0, 0, 0, 0, 0, 0}
+    });
+
+    @Test
+    void test() {
+        curr = new CurrBlock();
+        board.setBoard(initBoard(new int[][]{
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+                {0, 0, 0, 1, 0, 0, 1, 0, 0, 0},
+                {0, 0, 0, 1, 1, 0, 1, 1, 0, 0},
+                {0, 0, 1, 1, 1, 1, 1, 1, 0, 0}
+        }));
+
+        new BoomBlock().execute(board, curr, new int[] {17, 7});
+
+        assertThat(board.getBoard()).isDeepEqualTo(res);
+    }
+
+    UnitBlock[][] initBoard(int[][] bitBoard) {
+
+        UnitBlock[][] board = new UnitBlock[bitBoard.length][bitBoard[0].length];
+
+        for (int j = 0; j < bitBoard.length; j++) {
+            for (int i = 0; i < bitBoard[0].length; i++) {
+                if (bitBoard[j][i] == 1) {
+                    board[j][i] = new UnitBlock(Color.BLACK);
+                }
+            }
+        }
+        return board;
+    }
+
+}
