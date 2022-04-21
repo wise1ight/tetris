bf89b2384a5386a7de315fe41417d1a98e4cfbee
wlsh44 <k87913j@gmail.com>
2022-04-21 23:38:51 +0900

    feat: itemUnitBlock 추가 #42

---
+++ src/main/java/org/teamseven/tetris/block/item/ItemUnitBlock.java
@@ -0,0 +1,14 @@
+package org.teamseven.tetris.block.item;
+
+import org.teamseven.tetris.block.UnitBlock;
+
+import java.awt.*;
+
+public class ItemUnitBlock extends UnitBlock {
+
+    public ItemUnitBlock(Color color) {
+        super(color);
+    }
+
+
+}
