f4b7a3a330a28c582e96dfa79ba65934ec119531
김현욱 <company@wiselight.kr>
2022-04-22 03:43:30 +0900

    chore: 불필요한 부분 제거 #27

--- src/main/java/org/teamseven/tetris/ui/ScoreBoardTabbedPane.java
+++ src/main/java/org/teamseven/tetris/ui/ScoreBoardTabbedPane.java
@@ -1,48 +1,15 @@
 package org.teamseven.tetris.ui;
 
-import org.teamseven.tetris.Const;
 import org.teamseven.tetris.Pipeline;
-import org.teamseven.tetris.score.Score;
-import org.teamseven.tetris.handler.ScoreHandler;
 
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
-import java.awt.font.TextAttribute;
-import java.awt.font.TextLayout;
-import java.awt.geom.Rectangle2D;
-import java.text.AttributedString;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import static org.teamseven.tetris.Const.SCORE_ITEM_FILE;
 import static org.teamseven.tetris.Const.SCORE_NORMAL_FILE;
 
-public class ScoreBoardTabbedPane extends JTabbedPane implements  IDesign {
-    private JLabel lTime;
+public class ScoreBoardTabbedPane extends JTabbedPane implements IDesign {
     private ScoreBoardPanelTab noItem, item;
     private int[] preferredResolution;  // frame resolution - frame top border
-    private JLabel title;
-
-    private JPanel noItemScorePanelTab, itemScorePanelTab;
-    private JPanel titlePanel, buttonPanel;
-    private JPanel scorePanel1st, scorePanel2st, scorePanel3st, scorePanel4st, scorePanel5st,
-                    scorePanel6st, scorePanel7st, scorePanel8st, scorePanel9st, scorePanel10st;
-    private  JPanel[] scorePanels;
-    private JPanel line1st, line2st, line3st, line4st, line5st, line6st, line7st, line8st, line9st, line10st;
-
-    private CustomButton leftButton, rightButton;
-
-    private int pageNum = 0;
-    private int totalPageNum = 0;
-    private GridBagConstraints gridBagConstraints;
-    private GridBagLayout gridBagLayout;
-    private ScoreHandler handler = new ScoreHandler();
 
     public ScoreBoardTabbedPane() {
         int[] frameBorderSize = new int[2];       // frame top border
@@ -59,27 +26,18 @@ public class ScoreBoardTabbedPane extends JTabbedPane implements  IDesign {
 
     @Override
     public void setComp() {
-
         noItem = new ScoreBoardPanelTab(preferredResolution,SCORE_NORMAL_FILE);
         item = new ScoreBoardPanelTab(preferredResolution,SCORE_ITEM_FILE);
-
     }
 
     @Override
     public void setDesign() {
-        this.addTab("noitem",noItem);
-        this.addTab("item",item);
-
-
+        this.addTab("일반 게임 모드",noItem);
+        this.addTab("아이템 모드",item);
     }
 
     @Override
     public void setAction() {
-
-        this.setFocusable(false);
-        Score score3 = new Score(222, "AABB");
-        noItem.addScore(score3);
-
 
     }
 }
