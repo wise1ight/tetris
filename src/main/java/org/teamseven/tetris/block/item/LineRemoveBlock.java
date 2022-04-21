7426125c53425b54208b18f1ea6246d6ea3f1ce3
wlsh44 <k87913j@gmail.com>
2022-04-22 00:11:07 +0900

    feat: 라인 삭제 블럭 추가 #42

---
+++ src/main/java/org/teamseven/tetris/block/item/LineRemoveBlock.java
@@ -0,0 +1,10 @@
+package org.teamseven.tetris.block.item;
+
+import java.awt.*;
+
+public class LineRemoveBlock extends ItemUnitBlock {
+
+    public LineRemoveBlock(Color color) {
+        super(color);
+    }
+}
