89588e84f06fe5e5c15970281f1cabfebb4429c9
wlsh44 <k87913j@gmail.com>
2022-04-21 23:43:45 +0900

    feat: BoomBlock 추가 #42

---
+++ src/main/java/org/teamseven/tetris/block/item/BoomBlock.java
@@ -0,0 +1,10 @@
+package org.teamseven.tetris.block.item;
+
+import java.awt.*;
+
+public class BoomBlock extends ItemUnitBlock {
+
+    public BoomBlock(Color color) {
+        super(color);
+    }
+}
