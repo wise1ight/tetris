ed33f467ba461eaaa8014a37a776fcf87a952d45
isTest001 <91168781+isTest001@users.noreply.github.com>
2022-04-21 14:30:50 +0900

    chore

--- src/main/java/org/teamseven/tetris/ui/ScoreBoardPanelTab.java
+++ src/main/java/org/teamseven/tetris/ui/ScoreBoardPanelTab.java
@@ -5,20 +5,10 @@ import org.teamseven.tetris.score.ScoreHandler;
 
 import javax.swing.*;
 import javax.swing.border.Border;
-import javax.swing.border.CompoundBorder;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
-import java.awt.font.TextAttribute;
-import java.awt.font.TextLayout;
-import java.awt.geom.Rectangle2D;
-import java.text.AttributedString;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class ScoreBoardPanelTab extends JPanel implements  IDesign {
 
