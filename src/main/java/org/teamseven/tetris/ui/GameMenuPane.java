7f7e9edde3b277a3c48950df8015763ca95edc17
KimDa99 <91198933+KimDa99@users.noreply.github.com>
2022-05-01 22:33:59 +0900

    style : 메인메뉴 (전)팀 4 gui 가져옴

--- src/main/java/org/teamseven/tetris/ui/GameMenuPane.java
+++ src/main/java/org/teamseven/tetris/ui/GameMenuPane.java
@@ -1,5 +1,178 @@
 package org.teamseven.tetris.ui;
 
+
+import org.teamseven.tetris.Const;
+import org.teamseven.tetris.Pipeline;
+import org.teamseven.tetris.ui.*;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.HashSet;
+import java.util.Set;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameMenuPane extends Canvas {
+ /*
+    private JLabel title;
+
+    private CustomButton startButton, settingButton, scoreButton, exitButton, noItemModeButton, ItemModeButton, backButton;
+    private JButton settingBoard_ExitButton, scoreBoard_ExitButton;                         // exit button on popup panel
+    private JPanel main;                                                                    // main panel on startMenuPane (DEFAULT_LAYER)
+    //  private JPanel titlePanel, buttonPannel1, buttonPannel2, buttonPannel3, buttonPannel4;  // BoardLayout, panels on main panel
+    private JPanel buttonPannel1, buttonPannel2, buttonPannel3, buttonPannel4;  // BoardLayout, panels on main panel
+    private JPanel settingBoard, modeBoard;                                                // scoreBoard panel on startMenuPane (POPUP_LAYER)
+    private GridBagConstraints gridBagConstraints;
+    private GridBagLayout gridBagLayout;
+*/
+    private int[] preferredResolution;  // frame resolution - frame top border
+
+
+    public Frame homeFrame;
+    private Panel homePanel, titlePanel, buttonPanel;
+    static private int sizeInt = Pipeline.getSizeInt();
+
+    private Button start, item, scoreBoard, setting, exit;
+    private Button selected;
+    private List<Button> buttonList = new ArrayList<>();
+
+
+    public GameMenuPane() {
+        // for test
+        //  this.setOpaque(true);
+        //this.setBackground(Color.orange);
+
+        //  int[] frameBorderSize = new int[2];       // frame top border
+        //  frameBorderSize[0] = this.getInsets().left + this.getInsets().right;
+        //   frameBorderSize[1] = this.getInsets().top + this.getInsets().bottom;
+        preferredResolution = new int[2];
+        preferredResolution[0] = Pipeline.getScreenResolutionX();
+        preferredResolution[1] = Pipeline.getScreenResolutionY();
+
+        setDesign();
+        setAction();
+
+    }
+
+    public void setComp() {
+
+        setDesign();
+    }
+
+    public void setDesign() {
+        homeFrame = new Frame("Seoultech SE4 Tetris");
+        homeFrame.setSize(preferredResolution[0], preferredResolution[1]);
+        homeFrame.setResizable(true);
+        homeFrame.setLayout(null);
+        homeFrame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent windowEvent) {
+                System.exit(0);
+            }
+        });
+        homePanel = new Panel();
+        homePanel.setSize(preferredResolution[0], preferredResolution[1]);
+        homePanel.setBackground(Color.black);
+        homePanel.setLayout(null);
+        homePanel.setFont(new Font("Dialog", Font.PLAIN, sizeInt * 8));
+
+        titlePanel = new Panel();
+        titlePanel.setBounds(0, sizeInt * 50, preferredResolution[0], sizeInt * 40);
+        titlePanel.setFont(new Font("Dialog", Font.PLAIN, sizeInt * 25));
+
+        Label title = new Label("SE4 Tetris");
+        title.setForeground(Color.RED);
+
+        titlePanel.add(title);
+
+        buttonPanel = new Panel();
+        buttonPanel.setBounds(sizeInt * 110, sizeInt * 110, preferredResolution[0] - sizeInt * 110 * 2, sizeInt * 150);
+        buttonPanel.setLayout(new GridLayout(5, 1));
+
+        start = new Button("start");
+        scoreBoard = new Button("Scoreboard");
+        setting = new Button("Setting");
+        exit = new Button("Exit");
+
+        selected = start;
+        selected.setForeground(Color.gray);
+        buttonList.add(start);
+        buttonList.add(scoreBoard);
+        buttonList.add(setting);
+        buttonList.add(exit);
+
+        buttonPanel.add(start);
+        buttonPanel.add(scoreBoard);
+        buttonPanel.add(setting);
+        buttonPanel.add(exit);
+
+        homePanel.add(titlePanel);
+        homePanel.add(buttonPanel);
+
+        homeFrame.add(homePanel);
+
+        homeFrame.setVisible(true);
+
+        //   homeFrame.addKeyListener();
+        homeFrame.requestFocus();
+    }
+    
+    public void setAction(){
+        start.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                selected.setForeground(Color.black);
+                selected = start;
+                selected.setForeground(Color.gray);
+               // gameGUI.gameFrame.setVisible(true);
+            //   homeFrame.setVisible(false);
+            }
+        });
+        scoreBoard.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                selected.setForeground(Color.black);
+                selected = scoreBoard;
+                selected.setForeground(Color.gray);
+           //     scoreBoardGUI.scoreBoardFrame.setVisible(true);
+          //      homeFrame.setVisible(false);
+           //     scoreBoardGUI.scoreBoardFrame.requestFocus();
+            }
+        });
+
+        setting.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                selected.setForeground(Color.black);
+                selected = setting;
+                selected.setForeground(Color.gray);
+              //  settingPane.settingFrame.setVisible(true);
+            //    homeFrame.setVisible(false);
+            }
+        });
+
+        exit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+    
+        
+    }
+}
+
+
+
+
+/*
+
+
 import org.teamseven.tetris.Const;
 import org.teamseven.tetris.Pipeline;
 
@@ -234,3 +407,4 @@ public class GameMenuPane extends JLayeredPane implements IDesign {
 
 
 }
+ */
