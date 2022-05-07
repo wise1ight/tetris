936e0a2fab657a121a63175414813ba42b5a6ffc
김현욱 <company@wiselight.kr>
2022-05-07 20:46:00 +0900

    refactor: SettingPane에서 Comp, Design로 코드 분리 #62

--- src/main/java/org/teamseven/tetris/ui/SettingPane.java
+++ src/main/java/org/teamseven/tetris/ui/SettingPane.java
@@ -12,6 +12,7 @@ public class SettingPane extends JLayeredPane implements IDesign {
     private Panel settingPanel, buttonPanel, titlePanel;
     private Button home, keySetting, scoreBoard, color, difficulty_level, size, reSetting;
     private Button selected;
+    private Label title;
     private int sizeInt;
 
     public SettingPane() {
@@ -19,27 +20,19 @@ public class SettingPane extends JLayeredPane implements IDesign {
         setComp();
         setDesign();
         setAction();
+        refreshPref();
     }
 
     @Override
     public void setComp() {
         settingPanel = new Panel();
-        settingPanel.setSize(Pipeline.getScreenResolutionX(), Pipeline.getScreenResolutionY());
-        settingPanel.setBackground(Color.black);
-        settingPanel.setLayout(null);
-        settingPanel.setFont(new Font("Dialog", Font.PLAIN, sizeInt * 8));
 
         titlePanel = new Panel();
-        titlePanel.setBounds(0, sizeInt * 50, Pipeline.getScreenResolutionX(), sizeInt * 40);
-        titlePanel.setFont(new Font("Dialog", Font.PLAIN, sizeInt * 25));
-        Label title = new Label("Setting");
-        title.setForeground(Color.RED);
+        title = new Label("Setting");
 
         titlePanel.add(title);
 
         buttonPanel = new Panel();
-        buttonPanel.setBounds(sizeInt * 100, sizeInt * 110, Pipeline.getScreenResolutionX() - sizeInt * 100 *2 , sizeInt * 125);
-        buttonPanel.setLayout(new GridLayout(4, 2));
 
         keySetting = new Button("Key");
         scoreBoard = new Button("Reset Scoreboard");
@@ -63,7 +56,30 @@ public class SettingPane extends JLayeredPane implements IDesign {
         settingPanel.add(titlePanel);
 
         this.add(settingPanel);
+    }
 
+    @Override
+    public void setDesign() {
+        settingPanel.setSize(Pipeline.getScreenResolutionX(), Pipeline.getScreenResolutionY());
+        settingPanel.setBackground(Color.black);
+        settingPanel.setLayout(null);
+        settingPanel.setFont(new Font("Dialog", Font.PLAIN, sizeInt * 8));
+
+        titlePanel.setBounds(0, sizeInt * 50, Pipeline.getScreenResolutionX(), sizeInt * 40);
+        titlePanel.setFont(new Font("Dialog", Font.PLAIN, sizeInt * 25));
+
+        title.setForeground(Color.RED);
+
+        buttonPanel.setBounds(sizeInt * 100, sizeInt * 110, Pipeline.getScreenResolutionX() - sizeInt * 100 *2 , sizeInt * 125);
+        buttonPanel.setLayout(new GridLayout(4, 2));
+    }
+
+    @Override
+    public void setAction() {
+
+    }
+
+    private void refreshPref() {
         if (PreferencesHandler.getColorBlindnessType() == ColorBlindnessType.NONE) {
             color.setLabel("Color Blind Mode : OFF");
         } else {
@@ -73,64 +89,4 @@ public class SettingPane extends JLayeredPane implements IDesign {
         difficulty_level.setLabel("Level : " + PreferencesHandler.getMode());
         size.setLabel("Size : " + PreferencesHandler.getScreenSize());
     }
-
-    @Override
-    public void setDesign() {
-
-    }
-
-    @Override
-    public void setAction() {
-
-    }
-
-    /*
-    private void refreshPref() {
-        // Screen Size
-        switch (PreferencesHandler.getScreenSize()) {
-            case SMALL:
-                rbSmallSize.setSelected(true);
-                break;
-            case MEDIUM:
-                rbMediumSize.setSelected(true);
-                break;
-            case LARGE:
-                rbLargeSize.setSelected(true);
-                break;
-        }
-
-        // Game Mode
-        switch (PreferencesHandler.getMode()) {
-            case EASY:
-                rbEasy.setSelected(true);
-                break;
-            case NORMAL:
-                rbNormal.setSelected(true);
-                break;
-            case HARD:
-                rbHard.setSelected(true);
-                break;
-        }
-
-        // Keyboard
-        btnLeft.setText(KeyEvent.getKeyText(PreferencesHandler.getLeftBtnCode()));
-        btnRight.setText(KeyEvent.getKeyText(PreferencesHandler.getRightBtnCode()));
-        btnRotateRight.setText(KeyEvent.getKeyText(PreferencesHandler.getRotateRightBtnCode()));
-        btnHardDrop.setText(KeyEvent.getKeyText(PreferencesHandler.getHardDropBtnCode()));
-        btnSoftDrop.setText(KeyEvent.getKeyText(PreferencesHandler.getSoftDropBtnCode()));
-        btnPause.setText(KeyEvent.getKeyText(PreferencesHandler.getPauseBtnCode()));
-        btnExit.setText(KeyEvent.getKeyText(PreferencesHandler.getExitBtnCode()));
-
-        // Color Blindness
-        switch (PreferencesHandler.getColorBlindnessType()) {
-            case NONE:
-                rbNone.setSelected(true);
-                break;
-            case BLINDNESS:
-                rbColorBlindess.setSelected(true);
-                break;
-        }
-    }
-
-     */
 }
