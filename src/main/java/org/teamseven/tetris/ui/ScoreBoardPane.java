734b6cf9a3a35db183c2906a01043748c0fdc574
KimDa99 <hyhy61451@gmail.com>
2022-05-22 13:33:20 +0900

    refactor : scoreBoard 새로운 틀 추가

--- src/main/java/org/teamseven/tetris/ui/ScoreBoardPane.java
+++ src/main/java/org/teamseven/tetris/ui/ScoreBoardPane.java
@@ -1,3 +1,4 @@
+
 package org.teamseven.tetris.ui;
 
 import com.sun.org.apache.xpath.internal.operations.String;
@@ -19,7 +20,9 @@ import static org.teamseven.tetris.Const.SCORE_NORMAL_FILE;
 
 public class ScoreBoardPane extends JLayeredPane implements IDesign {
     private Label title;
-    private Panel scoreBoardPanel, normalScoreBoardTable, itemScoreBoardTable, buttonPanel, titlePanel;
+
+    private Label rank1,  rank2, rank3, rank4, rank5, rank6, rank7, rank8, rank9, rank10;
+    private Panel scoreBoardPanel, scoreBoardTable, buttonPanel, titlePanel;
 
     private Label rank1st, rank2nd;
     private Button home;
@@ -41,43 +44,40 @@ public class ScoreBoardPane extends JLayeredPane implements IDesign {
         title = new Label("ScoreBoard");
         titlePanel.add(title);
 
-        normalScoreBoardTable = new Panel();
-        itemScoreBoardTable = new Panel();
+        scoreBoardTable = new Panel();
 
         buttonPanel = new Panel();
 
         home = new Button("Back to Menu");
 
-
         buttonPanel.add(home);
 
-        rank1st = new Label("trying");
-        rank2nd = new Label("trying hard");
+        rank1 = new Label();
+        rank2 = new Label();
+        rank3 = new Label();
+        rank4 = new Label();
+        rank5 = new Label();
+        rank6 = new Label();
+        rank7 = new Label();
+        rank8 = new Label();
+        rank9 = new Label();
+        rank10 = new Label();
 
-
-       // normalScoreBoardTable.add(rank1st);
-     //   normalScoreBoardTable.add(rank2nd);
-   //     normalScoreBoardTable.add(new Label("test"));
-        for(int i = 0; i<10; i++) {
-
-            Label tmp = new Label(Integer.toString(i+1));
-            Label j = new Label(Integer.toString(i+10));
-            Label t = new Label(Integer.toString(i+100));
-
-            normalScoreBoardTable.add(tmp);
-            normalScoreBoardTable.add(j);
-            normalScoreBoardTable.add(t);
-
-            tmp.setForeground(Color.lightGray);
-            j.setForeground(Color.lightGray);
-            t.setForeground(Color.lightGray);
-        }
+        scoreBoardTable.add(rank1);
+        scoreBoardTable.add(rank2);
+        scoreBoardTable.add(rank3);
+        scoreBoardTable.add(rank4);
+        scoreBoardTable.add(rank5);
+        scoreBoardTable.add(rank6);
+        scoreBoardTable.add(rank7);
+        scoreBoardTable.add(rank8);
+        scoreBoardTable.add(rank9);
+        scoreBoardTable.add(rank10);
 
 
         scoreBoardPanel.add(buttonPanel);
         scoreBoardPanel.add(titlePanel);
-        scoreBoardPanel.add(normalScoreBoardTable);
-        scoreBoardPanel.add(itemScoreBoardTable);
+        scoreBoardPanel.add(scoreBoardTable);
 
         this.add(scoreBoardPanel);
     }
@@ -94,12 +94,11 @@ public class ScoreBoardPane extends JLayeredPane implements IDesign {
 
         title.setForeground(Color.RED);
 
-        normalScoreBoardTable.setBounds(sizeInt * 50, sizeInt * 60, Pipeline.getScreenResolutionX() - sizeInt * 50 , sizeInt * 160);
-        normalScoreBoardTable.setLayout(new GridLayout(10,3));
-        normalScoreBoardTable.setFont(new Font("Dialog", Font.PLAIN, sizeInt * 10));
+        scoreBoardTable.setBounds(sizeInt * 50, sizeInt * 60, Pipeline.getScreenResolutionX() - sizeInt * 50 , sizeInt * 160);
+        scoreBoardTable.setLayout(new GridLayout(10,1));
+        scoreBoardTable.setFont(new Font("Dialog", Font.PLAIN, sizeInt * 10));
 
-        rank1st.setForeground(Color.lightGray);
-        rank2nd.setForeground(Color.lightGray);
+        setScoreBoardFontColor(Color.lightGray);
 
         buttonPanel.setBounds(sizeInt * 50, sizeInt * 240, Pipeline.getScreenResolutionX() - sizeInt * 50 *2 , sizeInt * 80);
         buttonPanel.setLayout(new GridLayout(3, 5));
@@ -113,5 +112,18 @@ public class ScoreBoardPane extends JLayeredPane implements IDesign {
             }
         });
 
+    }
+
+    private void setScoreBoardFontColor(Color color){
+        rank1.setForeground(color);
+        rank2.setForeground(color);
+        rank3.setForeground(color);
+        rank4.setForeground(color);
+        rank5.setForeground(color);
+        rank6.setForeground(color);
+        rank7.setForeground(color);
+        rank8.setForeground(color);
+        rank9.setForeground(color);
+        rank10.setForeground(color);
     }
 }
